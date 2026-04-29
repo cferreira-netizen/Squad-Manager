2cf0341fefa7acc279a42a818c074bd862631359
cferreira-netizen <cferreira@caldwell.edu>
2026-04-29 17:21:22 +0000

    Apr 29, 2026, 1:21 PM

--- src/main.py
+++ src/main.py
@@ -4,3 +4,9 @@ import pandas as pd
 import streamlit as st
 from datetime import date
 
+# path helpers
+
+APP_PATH = os.path.dirname(os.path.abspath(__file__))
+
+def get_data_path():
+    """returns the absolute path to """
